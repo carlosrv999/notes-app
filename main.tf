5a0b9978cfea47043930782d1f8f2de4e8e3bea6
Carlos Ramirez <carlosrv125@icloud.com>
2022-08-24 21:21:45 -0500

    network added

---
+++ main.tf
@@ -0,0 +1,13 @@
+module "network" {
+  source = "./modules/network"
+
+  network_name = "vpc-carlos"
+  public_subnets = [
+    "10.100.0.0/21",
+    "10.100.8.0/21",
+    "10.100.16.0/21"
+  ]
+  public_subnet_suffix = "public-project-carlos"
+  region               = var.region
+
+}
---
+++ modules/network/outputs.tf
@@ -0,0 +1,11 @@
+output "network_id" {
+  value = google_compute_network.default.id
+}
+
+output "network_name" {
+  value = google_compute_network.default.name
+}
+
+output "public_subnets_names" {
+  value = google_compute_subnetwork.default[*].name
+}
